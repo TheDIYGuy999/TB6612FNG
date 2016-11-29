8d4a005c7d652b59963e0feb7f89b76c53df9a7b
TheDIYGuy999 <msommer74@gmail.com>
2016-11-29 17:20:10 +0100

    V1.1

--- examples/TB6612FNG/TB6612FNG.ino
+++ examples/TB6612FNG/TB6612FNG.ino
@@ -1,6 +1,8 @@
 // This example was cuccessfully tested on an Sparkfun Pro Micro in 8MHz version
 // It also works on a Pro Mini, but only, if you don't change the PWM frequency
 
+// Version 1.1
+
 //
 // =======================================================================================================
 // INCLUDE LIRBARIES
@@ -17,8 +19,8 @@
 // =======================================================================================================
 //
 
-// Initialize TB6612FNG H-Bridge
-#define motor1_in1 2 // define motor pin numbers
+// define motor pin numbers
+#define motor1_in1 2
 #define motor1_in2 4
 #define motor1_pwm 5
 
@@ -26,10 +28,42 @@
 #define motor2_in2 8
 #define motor2_pwm 6
 
-// SYNTAX: IN1, IN2, PWM, min. input value, max. input value, neutral position width
-// invert rotation direction true or false
-TB6612FNG Motor1(motor1_in1, motor1_in2, motor1_pwm, 0, 1023, 60, false); // Motor 1
-TB6612FNG Motor2(motor2_in1, motor2_in2, motor2_pwm, 0, 1023, 60, false); // Motor 2
+// Create motor objects
+TB6612FNG Motor1; // Motor 1
+TB6612FNG Motor2; // Motor 2
+
+//
+// =======================================================================================================
+// LIGHTS
+// =======================================================================================================
+//
+
+void lights() {
+  if (Motor1.brakeActive()) { // if braking detected from TB6612FNG motor driver
+    //digitalWrite(BRAKELIGHTS, HIGH);
+  }
+  else {
+    //digitalWrite(BRAKELIGHTS, LOW);
+  }
+}
+
+//
+// =======================================================================================================
+// MAIN ARDUINO SETUP (1x during startup)
+// =======================================================================================================
+//
+void setup() {
+
+  // Motor pin setup
+  // SYNTAX: IN1, IN2, PWM, min. input value, max. input value, neutral position width
+  // invert rotation direction true or false
+  Motor1.begin(motor1_in1, motor1_in2, motor1_pwm, 0, 1023, 60, false); // Motor 1
+  Motor2.begin(motor2_in1, motor2_in2, motor2_pwm, 0, 1023, 60, false); // Motor 2
+
+  // Optional: Motor PWM frequency (Requires the PWMFrequency.h library)
+  /*setPWMPrescaler(motor1_pwm, 1); // 123Hz = 256,  492Hz = 64, 3936Hz = 8, 31488Hz = 1
+    setPWMPrescaler(motor2_pwm, 1);*/
+}
 
 //
 // =======================================================================================================
@@ -53,32 +87,6 @@ void driveMotor() {
     // returns true, if motor is running, so we can do other stuff here!
   }
   Motor2.drive(speed2, 255, 1, false); // Motor 2
-}
-
-//
-// =======================================================================================================
-// LIGHTS
-// =======================================================================================================
-//
-
-void lights() {
-  if (Motor1.brakeActive()) { // if braking detected from TB6612FNG motor driver
-       //digitalWrite(BRAKELIGHTS, HIGH);
-    }
-    else {
-      //digitalWrite(BRAKELIGHTS, LOW);
-    }
-}
-
-//
-// =======================================================================================================
-// MAIN ARDUINO SETUP (1x during startup)
-// =======================================================================================================
-//
-void setup() {
-  // Optional: Motor PWM frequency (Requires the PWMFrequency.h library)
-  /*setPWMPrescaler(motor1_pwm, 1); // 123Hz = 256,  492Hz = 64, 3936Hz = 8, 31488Hz = 1
-    setPWMPrescaler(motor2_pwm, 1);*/
 }
 
 //
